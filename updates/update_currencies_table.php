fc3d02bd2881fbb8c8a599bec28b0bd0765146bc
kernel <1@crownet.ru>
2016-10-07 19:02:50 +0400

    add minimum field

---
+++ updates/update_currencies_table.php
@@ -0,0 +1,23 @@
+<?php namespace Responsiv\Currency\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class UpdateCurrenciesTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('kernel_booking_apartament', function($table)
+        {
+            $table->integer('minimum')->nullable(false)->unsigned()->default(null);
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('kernel_booking_apartament', function($table)
+        {
+            $table->dropColumn('miniumum');
+        });
+    }
+}
